45b2a4cebc4339fa8ca0dc17430f563ad1671d05
epixhints <jeffrey.arneson@gmail.com>
2013-02-12 17:43:51 -0800

    Add path of exile objects for json parsing and stub of usable Item Object

---
+++ ExiledToolkit/ExiledToolkit/Models/PathOfExileObjects.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExiledToolkit.Models
+{
+    namespace PathOfExileObjects
+    {
+        public class StashTab
+        {
+            public List<Item> items;
+            public int numTabs;
+        }
+
+        public class Item
+        {
+            public bool verified;
+            public int w;
+            public int h;
+            public String icon;
+            public bool support;
+            public String league;
+            public List<Object> sockets;
+            public String name;
+            public String typeLine;
+            public bool identified;
+            public List<Property> properties;
+            public List<Property> requirements;
+            public List<String> implicitMods;
+            public List<String> explicitMods;
+            public String descrText;
+            public int frameType;
+            public int x;
+            public int y;
+            public String inventoryId;
+            public List<Item> socketedItems;
+        }
+
+        public class Property
+        {
+            public String name;
+            public List<String[]> values;
+            public int displayMode;
+        }
+    }
+}
---
+++ ExiledToolkit/ExiledToolkit/Models/ToolkitObjects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExiledToolkit.Models
+{
+    namespace ToolkitObjects
+    {
+        class Item
+        {
+            public Item()
+            {
+            }
+
+            public Item(ExiledToolkit.Models.PathOfExileObjects.Item pItem)
+            {
+
+            }
+        }
+    }
+}
